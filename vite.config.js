b3b301e3b32abcdcf50d0bdaf72e8db0f981d39f
AiboCulture <abelzou60@gmail.com>
2026-02-14 12:11:31 +0800

    指向修复

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
+import { resolve } from 'path'
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        addressProofCase: resolve(__dirname, 'address-proof-case.html'),
+        applyVcc: resolve(__dirname, 'apply-vcc.html'),
+        collect: resolve(__dirname, 'collect.html'),
+        cryptopay: resolve(__dirname, 'cryptopay.html'),
+        customPage: resolve(__dirname, 'custom-page.html'),
+        eCnyGuide: resolve(__dirname, 'e-cny-guide.html'),
+        liftAccountRestriction: resolve(__dirname, 'lift-account-restriction.html'),
+        moneyFromChina: resolve(__dirname, 'money-from-China.html'),
+        otherEvaluation: resolve(__dirname, 'other-evaluation.html'),
+        pageCase: resolve(__dirname, 'page-case.html'),
+        paymentsPure: resolve(__dirname, 'payments-pure.html'),
+        payments: resolve(__dirname, 'payments.html'),
+        reviews: resolve(__dirname, 'reviews.html'),
+        wiseDepositCase: resolve(__dirname, 'wise-deposit-case.html')
+      }
+    }
+  },
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
